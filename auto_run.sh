1f9349200299bf2b8d77d567b0ba8bbd68d924fd
shanjin.yang <shanjin.yang@aispeech.com>
2016-12-07 16:49:40 +0800

    improve shell code
    Signed-off-by: shanjin.yang <shanjin.yang@aispeech.com>

--- auto_run.sh
+++ auto_run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh  
 Folder_TEST="/home/jim/Downloads/dao_hang"
 for file_test in ${Folder_TEST}/*; do
-    echo $(file_test)
-    /home/jim/test/adpcm/bin/adpcm -e:pcm 1 dvi4 -i $file_test -o test.adpcm
-    /home/jim/test/adpcm/bin/adpcm -d:dvi4 1 pcm -i test.adpcm -o test.pcm
+    echo $file_test
+    /home/jim/test/adpcm/bin/adpcm -e -i $file_test -o test.adpcm
+    /home/jim/test/adpcm/bin/adpcm -d -i test.adpcm -o test.pcm
     /opt/aispeech/linux_asr/aispeech-speex-ogg-c-sample-20151116/fos test.pcm
 done
