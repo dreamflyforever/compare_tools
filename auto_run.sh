492f5ff909560ef41d5ee3122ca7cf6a3c58c155
shanjin.yang <shanjin.yang@aispeech.com>
2016-12-08 16:03:12 +0800

    add test case
    Signed-off-by: shanjin.yang <shanjin.yang@aispeech.com>

--- auto_run.sh
+++ auto_run.sh
@@ -7,13 +7,66 @@ for file_test in ${Folder_TEST}/*; do
 	/opt/aispeech/linux_asr/aispeech-speex-ogg-c-sample-20151116/fos test.pcm
 done
 
-#!/bin/sh  
 Folder_TEST="/home/jim/Downloads/xia_yi_shou"
 for file_test in ${Folder_TEST}/*; do
 	echo $file_test
 	#44k covenr to 16k
 	resample -to 16000 $file_test test.wav
 	/home/jim/test/adpcm/bin/adpcm -e -i test.wav -o test.adpcm
+	/home/jim/test/adpcm/bin/adpcm -d -i test.adpcm -o test.pcm
+	/opt/aispeech/linux_asr/aispeech-speex-ogg-c-sample-20151116/fos test.pcm
+done
+
+Folder_TEST="/home/jim/Downloads/data_wkwrd/2d_mo_shi"
+for file_test in ${Folder_TEST}/*; do
+	echo $file_test
+	/home/jim/test/adpcm/bin/adpcm -e -i $file_test -o test.adpcm
+	/home/jim/test/adpcm/bin/adpcm -d -i test.adpcm -o test.pcm
+	/opt/aispeech/linux_asr/aispeech-speex-ogg-c-sample-20151116/fos test.pcm
+done
+
+Folder_TEST="/home/jim/Downloads/data_wkwrd/che_tou_xiang_shang"
+for file_test in ${Folder_TEST}/*; do
+	echo $file_test
+	#44k covenr to 16k
+	resample -to 16000 $file_test test.wav
+	/home/jim/test/adpcm/bin/adpcm -e -i test.wav -o test.adpcm
+	/home/jim/test/adpcm/bin/adpcm -d -i test.adpcm -o test.pcm
+	/opt/aispeech/linux_asr/aispeech-speex-ogg-c-sample-20151116/fos test.pcm
+done
+
+Folder_TEST="/home/jim/Downloads/data_wkwrd/duo_bi_xian_xing"
+for file_test in ${Folder_TEST}/*; do
+	echo $file_test
+	/home/jim/test/adpcm/bin/adpcm -e -i $file_test -o test.adpcm
+	/home/jim/test/adpcm/bin/adpcm -d -i test.adpcm -o test.pcm
+	/opt/aispeech/linux_asr/aispeech-speex-ogg-c-sample-20151116/fos test.pcm
+done
+
+Folder_TEST="/home/jim/Downloads/data_wkwrd/guan_bi_ping_mu"
+for file_test in ${Folder_TEST}/*; do
+	echo $file_test
+	#44k covenr to 16k
+	resample -to 16000 $file_test test.wav
+	/home/jim/test/adpcm/bin/adpcm -e -i test.wav -o test.adpcm
+	/home/jim/test/adpcm/bin/adpcm -d -i test.adpcm -o test.pcm
+	/opt/aispeech/linux_asr/aispeech-speex-ogg-c-sample-20151116/fos test.pcm
+done
+
+Folder_TEST="/home/jim/Downloads/data_wkwrd/guan_bi_sheng_yin"
+for file_test in ${Folder_TEST}/*; do
+	echo $file_test
+	#44k covenr to 16k
+	resample -to 16000 $file_test test.wav
+	/home/jim/test/adpcm/bin/adpcm -e -i test.wav -o test.adpcm
+	/home/jim/test/adpcm/bin/adpcm -d -i test.adpcm -o test.pcm
+	/opt/aispeech/linux_asr/aispeech-speex-ogg-c-sample-20151116/fos test.pcm
+done
+
+Folder_TEST="/home/jim/Downloads/data_wkwrd/hai_yao_duo_jiu"
+for file_test in ${Folder_TEST}/*; do
+	echo $file_test
+	/home/jim/test/adpcm/bin/adpcm -e -i $file_test -o test.adpcm
 	/home/jim/test/adpcm/bin/adpcm -d -i test.adpcm -o test.pcm
 	/opt/aispeech/linux_asr/aispeech-speex-ogg-c-sample-20151116/fos test.pcm
 done
