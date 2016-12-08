65ab8771f9a00e7bc209b7b8e31299713b52fef9
shanjin.yang <shanjin.yang@aispeech.com>
2016-12-08 15:16:15 +0800

    add resample function
    sample 44k cover to 16k
    
    Signed-off-by: shanjin.yang <shanjin.yang@aispeech.com>

--- auto_run.sh
+++ auto_run.sh
@@ -1,8 +1,19 @@
 #!/bin/sh  
 Folder_TEST="/home/jim/Downloads/dao_hang"
 for file_test in ${Folder_TEST}/*; do
-    echo $file_test
-    /home/jim/test/adpcm/bin/adpcm -e -i $file_test -o test.adpcm
-    /home/jim/test/adpcm/bin/adpcm -d -i test.adpcm -o test.pcm
-    /opt/aispeech/linux_asr/aispeech-speex-ogg-c-sample-20151116/fos test.pcm
+	echo $file_test
+	/home/jim/test/adpcm/bin/adpcm -e -i $file_test -o test.adpcm
+	/home/jim/test/adpcm/bin/adpcm -d -i test.adpcm -o test.pcm
+	/opt/aispeech/linux_asr/aispeech-speex-ogg-c-sample-20151116/fos test.pcm
+done
+
+#!/bin/sh  
+Folder_TEST="/home/jim/Downloads/xia_yi_shou"
+for file_test in ${Folder_TEST}/*; do
+	echo $file_test
+	#44k covenr to 16k
+	resample -to 16000 $file_test test.wav
+	/home/jim/test/adpcm/bin/adpcm -e -i test.wav -o test.adpcm
+	/home/jim/test/adpcm/bin/adpcm -d -i test.adpcm -o test.pcm
+	/opt/aispeech/linux_asr/aispeech-speex-ogg-c-sample-20151116/fos test.pcm
 done
