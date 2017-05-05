e0292ad0429d3f68f525b99d22330cb7135bf85e
dreamflyforever <sjyangv0@qq.com>
2017-05-05 21:02:39 +0800

    mv *.ogg & *.wav to OGG & WAV
    Signed-off-by: dreamflyforever <sjyangv0@qq.com>

--- 8k_2_16k_ogg.sh
+++ 8k_2_16k_ogg.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 Folder_TEST="/home/jim/Downloads/out"
-WAV=".wav"
-OGG=".ogg"
+EWAV="_8k.wav"
+EOGG="_8k.ogg"
 SKOGG="_16k.ogg"
 for file_test in ${Folder_TEST}/*; do
 	echo $file_test
 	FILE_NAME=$(basename "$file_test" .wav)
 	echo $FILE_NAME
-	resample -to 8000 $file_test $FILE_NAME$WAV
-	/home/jim/test/compare_tools/program/speexenc $FILE_NAME$WAV $FILE_NAME$OGG
-	ffmpeg -i $FILE_NAME$OGG -ar 16000 $FILE_NAME$SKOGG
+	resample -to 8000 $file_test $FILE_NAME$EWAV
+	/home/jim/test/compare_tools/program/speexenc $FILE_NAME$EWAV $FILE_NAME$EOGG
+	ffmpeg -i $FILE_NAME$EOGG -ar 16000 $FILE_NAME$SKOGG
 	#/home/jim/test/compare_tools/program/check_aihome.py
 done
+mkdir OGG
+mkdir WAV
+mv *.ogg OGG
+mv *.wav WAV
