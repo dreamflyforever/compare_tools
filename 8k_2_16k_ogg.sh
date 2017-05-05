af9c8b5a60af8b63250d2b9e36226a08d17061ce
dreamflyforever <sjyangv0@qq.com>
2017-05-05 20:58:52 +0800

    keep the 8k wav & 8k speex ogg & 16k speex ogg
    Signed-off-by: dreamflyforever <sjyangv0@qq.com>

--- 8k_2_16k_ogg.sh
+++ 8k_2_16k_ogg.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 Folder_TEST="/home/jim/Downloads/out"
+WAV=".wav"
+OGG=".ogg"
+SKOGG="_16k.ogg"
 for file_test in ${Folder_TEST}/*; do
 	echo $file_test
-	resample -to 8000 $file_test test.wav
-	/home/jim/test/compare_tools/program/speexenc test.wav test.ogg
-	 ffmpeg -i test.ogg -ar 16000 song.ogg
-	/home/jim/test/compare_tools/program/check_aihome.py
-	rm test.wav
-	rm song.ogg
-	rm test.ogg
+	FILE_NAME=$(basename "$file_test" .wav)
+	echo $FILE_NAME
+	resample -to 8000 $file_test $FILE_NAME$WAV
+	/home/jim/test/compare_tools/program/speexenc $FILE_NAME$WAV $FILE_NAME$OGG
+	ffmpeg -i $FILE_NAME$OGG -ar 16000 $FILE_NAME$SKOGG
+	#/home/jim/test/compare_tools/program/check_aihome.py
 done
