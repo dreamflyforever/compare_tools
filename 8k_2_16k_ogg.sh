9884856655e93774a51ac15759da14c43f7dfe0f
dreamflyforever <sjyangv0@qq.com>
2017-05-08 20:06:23 +0800

    16k wav --> 8k wav --> 8k ogg --> 8k wav --> 16k wav --> 16 ogg
    Signed-off-by: dreamflyforever <sjyangv0@qq.com>

--- 8k_2_16k_ogg.sh
+++ 8k_2_16k_ogg.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+rm OGG
+rm WAV
+
 Folder_TEST="/home/jim/Downloads/out"
 EWAV="_8k.wav"
 EOGG="_8k.ogg"
+SEWAV="__8k.wav"
+SWAV="_16k.wav"
 SKOGG="_16k.ogg"
 for file_test in ${Folder_TEST}/*; do
 	echo $file_test
@@ -9,10 +14,21 @@ for file_test in ${Folder_TEST}/*; do
 	echo $FILE_NAME
 	resample -to 8000 $file_test $FILE_NAME$EWAV
 	/home/jim/test/compare_tools/program/speexenc $FILE_NAME$EWAV $FILE_NAME$EOGG
-	ffmpeg -i $FILE_NAME$EOGG -ar 16000 $FILE_NAME$SKOGG
-	#/home/jim/test/compare_tools/program/check_aihome.py
+	/home/jim/test/compare_tools/program/speexdec $FILE_NAME$EOGG $FILE_NAME$SEWAV
+	resample -to 16000 $FILE_NAME$SEWAV $FILE_NAME$SWAV
+	/home/jim/test/compare_tools/program/speexenc $FILE_NAME$SWAV $FILE_NAME$SKOGG
+	cp $FILE_NAME$SKOGG song.ogg
+	/home/jim/test/compare_tools/program/check_aihome.py
 done
 mkdir OGG
 mkdir WAV
 mv *.ogg OGG
 mv *.wav WAV
+
+echo "--------------------------------------------------------"
+
+
+for file_test in ${Folder_TEST}/*; do
+	mv $file_test song.ogg
+	/home/jim/test/compare_tools/program/check_aihome.py
+done
